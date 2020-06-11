efa4bfbbaceade2a012f65f814dbc326d6bd010a
Aasa-Christian <aasachristian@yahoo.com>
2020-06-10 21:06:15 -0400

    completed mvp

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,48 @@
+from util import Stack, Queue  # These may come in handy
 
+test_ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
+
+def get_ancestor(ancestors, child):
+    hiers = []
+    for hier in ancestors:
+        if hier[1] == child:
+            hiers.append(hier[0])
+    return hiers
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    q = Queue()
+    q.enqueue([starting_node])
+    visited = set()
+    path_len = 1
+    oldest_parent = -1
+    while q.size() > 0:
+        path = q.dequeue()
+        cur_node = path[-1]
+
+        if cur_node not in visited:
+            visited.add(cur_node)
+
+        if len(path) >= path_len and cur_node < oldest_parent or len(path) > path_len:
+            path_len = len(path)
+            oldest_parent = cur_node
+
+        for father in get_ancestor(ancestors, cur_node):
+            path_copy = list(path)
+            path_copy.append(father)
+            q.enqueue(path_copy)
+
+    return oldest_parent
+
+# print(earliest_ancestor(test_ancestors, 1))
+# print(earliest_ancestor(test_ancestors, 2))
+# print(earliest_ancestor(test_ancestors, 3))
+# print(earliest_ancestor(test_ancestors, 4))
+# print(earliest_ancestor(test_ancestors, 5))
+# print(earliest_ancestor(test_ancestors, 6))
+# print(earliest_ancestor(test_ancestors, 7))
+# print(earliest_ancestor(test_ancestors, 8))
+# print(earliest_ancestor(test_ancestors, 9))
+
+
+
+
+
--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -56,16 +56,46 @@ class Graph:
         Print each vertex in depth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
+        s = Stack()
+        s.push(starting_vertex)
+        visited = set()
 
-    def dft_recursive(self, starting_vertex):
+        while s.size() > 0:
+            v = s.pop()
+
+            if v not in visited:
+                print(v)
+                visited.add(v)
+
+                for next_vert in self.get_neighbors(v):
+                    s.push(next_vert)
+
+    def dft_recursive(self, starting_vertex, visit = None):
         """
         Print each vertex in depth-first order
         beginning from starting_vertex.
 
         This should be done using recursion.
         """
-        pass  # TODO
+        s = Stack()
+        s.push([starting_vertex])
+        if visit == None:
+            visit = {}
+            visited = visit
+        else:
+            visited = visit
+        while starting_vertex not in visited:
+            # print(visited, "visited")
+            print(starting_vertex)
+            visited[starting_vertex] = starting_vertex
+            for n in self.get_neighbors(starting_vertex):
+                if n not in visited:     
+                    return self.dft_recursive(n, visited)
+         
+        
+
+
+        
 
     def bfs(self, starting_vertex, destination_vertex):
         """
@@ -75,17 +105,17 @@ class Graph:
         """
         # Create an empty queue and enqueue A PATH TO the starting vertex ID
         q = Queue()
-        q.enqueue(starting_vertex)
+        q.enqueue([starting_vertex])
 		# Create a Set to store visited vertices
         vistited = set()
+        
 		# While the queue is not empty...
         while q.size() > 0:
-            print(q.size(), "q.size")
 			# Dequeue the first PATH
             path = q.dequeue()
-            print(path, "path")
-			# Grab the last vertex from the PATH
             v = path[-1]
+			# Grab the last vertex from the PATH
+
 			# If that vertex has not been visited...
             if v not in vistited:
 				# CHECK IF IT'S THE TARGET
@@ -101,6 +131,7 @@ class Graph:
                     q.enqueue(path_copy)
 				  # COPY THE PATH
 				  # APPEND THE NEIGHOR TO THE BACK
+        return None
 
     def dfs(self, starting_vertex, destination_vertex):
         """
@@ -108,7 +139,39 @@ class Graph:
         starting_vertex to destination_vertex in
         depth-first order.
         """
-        pass  # TODO
+        # Create an empty queue and enqueue A PATH TO the starting vertex ID
+        s = Stack()
+        s.push([starting_vertex])
+		# Create a Set to store visited vertices
+        vistited = set()
+        
+		# While the queue is not empty...
+        while s.size() > 0:
+            # print(s.size(), "s.size")
+			# Dequeue the first PATH
+            path = s.pop()
+            
+            # print(path, "path 88")
+
+            v = path[-1]
+			# Grab the last vertex from the PATH
+
+			# If that vertex has not been visited...
+            if v not in vistited:
+				# CHECK IF IT'S THE TARGET
+                if v == destination_vertex:
+				  # IF SO, RETURN PATH
+                    return path
+                vistited.add(v)
+				# Mark it as visited...
+				# Then add A PATH TO its neighbors to the back of the queue
+                for neighbors in self.get_neighbors(v):
+                    path_copy = list(path)
+                    path_copy.append(neighbors)
+                    s.push(path_copy)
+				  # COPY THE PATH
+				  # APPEND THE NEIGHOR TO THE BACK
+        return None
 
     def dfs_recursive(self, starting_vertex, destination_vertex):
         """
